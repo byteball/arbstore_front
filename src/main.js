945717434fab758d137da540e05954204ac9eb53
kakysha <kakysha@me.com>
2022-02-21 18:59:41 +0300

    google analytics

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Notifications from '@kyvg/vue3-notification'
+import VueGtag from "vue-gtag";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import './assets/css/fancybox.min.css';
 import './assets/css/main.css';
 
-createApp(App).use(store).use(router).use(Notifications).mount('#app')
+const app = createApp(App).use(store).use(router).use(Notifications);
+if (!window.location.hostname.includes('testnet.')) {
+	app.use(VueGtag, {
+		config: { id: "G-CM0KLFYDWV" }
+	}, router);
+}
+app.mount('#app')
 
 
