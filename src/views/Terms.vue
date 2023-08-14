f009deaf08ab6a9c6cb0e97228661ab376400faf
Alexander Ponomarev <taump@yandex.ru>
2023-08-14 19:21:43 +0500

    add new pages & links in the footer (#8)
    * feat: add new pages & links in the footer
    
    * fix: add &amp;

---
+++ src/views/Terms.vue
@@ -0,0 +1,292 @@
+<template>
+  <main class="mt-5 terms">
+    <div class="container mt-50 pb-5">
+      <div class="f-52 ff-2b mb-2">Terms &amp; Conditions</div>
+      <div class="row mt-5">
+        <div class="col-lg-12 f-18 color-2">
+          <ol type="1">
+            <li>
+              ArbStore
+              <p>
+                ArbStore, the legal entity described in <a href="https://arbstore.org">arbstore.org</a>,
+                supports cross-border trade and services with arbitration for
+                P2P crypto payments by facilitating dispute resolution .
+                ArbStore links parties who have entered a transaction that might
+                eventually result in a commercial or legal dispute (the
+                <i>“Parties”</i>) with arbitrators for the resolution of their
+                disputes via smart contracts. For the purpose of these Terms and
+                Conditions, arbitration means the online dispute resolution
+                service facilitated by ArbStore and regulated by the terms of
+                this Terms and Conditions and the terms described in section
+                <i>“User guide for contracts with arbitration”</i> section in
+                ArbStore’s page (<a href="https://arbstore.org/guide">arbstore.org/guide</a>) (the
+                <i>“Guide”</i>). To the extent permitted by public policy,
+                arbitration facilitated by ArbStore is not subject to any other
+                arbitration rules, conventions or treaties.
+              </p>
+            </li>
+
+            <li>
+              The Parties
+              <p>
+                The Parties wish to enter an online transaction using a smart
+                contract with arbitration (the <i>“Transaction”</i>) and, should
+                they have a dispute (the <i>“Dispute”</i>), resolve it in
+                accordance with the Guide. Accordingly, the buyer in the
+                Transaction agrees to deposit the transaction funds, in
+                cryptocurrency, (the <i>“Transaction Amount”</i>) in a Smart
+                Contract created by the Parties for the Transaction, in escrow,
+                pending satisfactory completeness of the Transaction.
+              </p>
+
+              <p>
+                The Parties appoint an arbitrator out of those listed on
+                ArbStore’s website (the “Arbitrator”) to resolve the potential
+                Dispute. The Parties are responsible to select an Arbitrator
+                who, according to the Arbitrator’s description in ArbStore, is
+                qualified or has the adequate level of expertise required to
+                solve the potential Dispute. The Parties are recommended to
+                reach out and confirm directly with the Arbitrator that he/she
+                has the adequate qualifications or level of expertise to resolve
+                the potential Dispute. The Arbitrator may charge an additional
+                fee for this consultation.
+              </p>
+
+              <p>
+                If the selected Arbitrator does not feel that he/she has the
+                right qualification or level of expertise, the Arbitrator may
+                decline the appointment and the Parties shall select another
+                Arbitrator within ArbStore or find another way of concluding
+                their contract with another dispute resolution method.
+              </p>
+              <p>
+                By selecting the Arbitrator, the Parties agree to the
+                Arbitrator’s fees and other terms described in the Arbitrator’s
+                profile.
+              </p>
+            </li>
+            <li>
+              Escrow
+              <p>
+                The Transaction Amount will be kept in escrow, in the smart
+                contract, by the Parties until the Transaction is satisfactorily
+                completed, in which case the Parties shall release the
+                Transaction Amount from the smart contract to their
+                counterparty, and the seller shall get paid, or the buyer shall
+                get refunded, in accordance with the Guide. If the Dispute
+                arises, the Arbitrator will resolve the Dispute and will make
+                the corresponding funds available to the winning party.
+              </p>
+              <p>
+                Neither ArbStore nor the Arbitrator will be holding the
+                Transaction Amount on behalf of the Parties, nor will they
+                maintain the Transaction Amount or any other funds in custody
+                for the Parties
+              </p>
+            </li>
+            <li>
+              Arbitrators
+              <p>
+                The Arbitrator is either a legal professional or has technical
+                or commercial knowledge that allows him/her to resolve legal,
+                technical or commercial disputes. The Arbitrator in ArbStore can
+                also be a legal entity composed of several real persons and
+                represented by a single person or a group of persons (via a
+                multi-signature wallet).
+              </p>
+              <p>
+                The Arbitrator has been admitted to join the ArbStore
+                arbitrators’ cast. Should the Dispute arise, the party claiming
+                the breach (the <i>“Claimant”</i>) may instruct the Arbitrator
+                to resolve the dispute.
+              </p>
+              <p>
+                Arbitrators shall keep their profile in ArbStore updated, true
+                and accurate at all times.
+              </p>
+              <p>
+                Arbitrations may review their proposed pricing in ArbStore at
+                any time but they shall respect the fees that were active during
+                creation of the disputed contract.
+              </p>
+              <p>
+                No other third parties shall be included in these Terms and
+                Conditions unless prior written consent between the parties has
+                been obtained.
+              </p>
+              <p>
+                Arbitrators shall remove themselves from the Arbitrators’ list
+                if they expect to be unavailable from some time.
+              </p>
+              <p>
+                ArbStore shall be entitled to remove Arbitrators from the
+                Arbitrators’ list, at any time.
+              </p>
+            </li>
+
+            <li>
+              Arbitration Process and Pleadings
+              <p>
+                Generally, the arbitration process, in equity or in law, will
+                follow the steps described in ArbStore’s webpage. Arbitrators
+                may change the process as they deem appropriate
+              </p>
+            </li>
+
+            <li>
+              Conflict of Interests
+              <p>
+                The Parties declare that they do not have any common interest or
+                relationship with the chosen Arbitrator that may bias or
+                influence the Arbitrators’ judgment to resolve a potential
+                Dispute.
+              </p>
+            </li>
+
+            <li>
+              Arbitrator’s representation
+              <p>
+                The Arbitrator commits to resolve the Dispute in the terms
+                described in the Guide and the Arbitrator’s profile on ArbStore
+                and represents that he/she has the technical and legal
+                competence required to resolve disputes in the fields the
+                Arbitrator indicated in their profile on ArbStore and shall
+                resolve the dispute with professional diligence.
+              </p>
+            </li>
+
+            <li>
+              Costs and Fees
+              <p>
+                The seller will pay ArbStore 0.75% of the Transaction Amount.
+              </p>
+              <p>
+                Arbitration fees, as indicated in the Arbitrator’s profile shall
+                be paid by the Claimant to the Arbitrator as described in the
+                Guide.
+              </p>
+            </li>
+
+            <li>
+              Waiver
+              <p>
+                By entering these Terms and Conditions, the Parties commit to
+                submit the Dispute to resolution by the Arbitrator. To the
+                extent permitted by applicable laws and public policy, the
+                Parties waive their rights to take the Dispute to a court of
+                justice and to appeal the Arbitrator’s decision resolving the
+                Dispute (the <i>“Award”</i>).
+              </p>
+            </li>
+
+            <li>
+              Confidentiality
+              <p>
+                Until the Award is issued, neither party may reveal any
+                information pertaining to the Terms and Conditions or related to
+                the Dispute or the arbitration proceedings publicly.
+              </p>
+              <p>
+                Once the Award is issued, it will be registered in the Obyte
+                direct acyclic graph (DAG). Details of the Transaction and the
+                Dispute shall remain confidential. Such details may not be
+                disclosed to third parties without written approval from both
+                Parties, ArbStore and the Arbitrator.
+              </p>
+            </li>
+
+            <li>
+              Privacy
+              <p>
+                The Arbitrator agrees to join ArbStore’s <router-link to="/privacy">Privacy Policy</router-link> as a
+                data controller. The Parties grant consent to ArbStore and the
+                Arbitrator to collect and treat their personal data in
+                accordance with ArbStore’s <router-link to="/privacy">Privacy Policy</router-link> for the purposes of
+                resolving the Dispute.
+              </p>
+            </li>
+
+            <li>
+              Representations
+              <p>
+                The Parties represent that they have the legal capacity to enter
+                into these Terms and Conditions and that the Transaction and its
+                subject matter is legitimate and does not violate any applicable
+                laws.
+              </p>
+              <p>
+                The Arbitrator shall refuse to grant an Award should he/she
+                suspect that the Parties have entered this Terms and Conditions
+                in breach of this representation.
+              </p>
+            </li>
+            <li>
+              Termination/Force majeure/Impossibility to provide the service
+              <p>
+                The Arbitrator’s obligation to resolve the Dispute will
+                extinguish in case of force majeure, inability of the Arbitrator
+                to deliver the Award, by any reason, or any other circumstance
+                that makes resolution of the Dispute impossible (e.g. loss of
+                keys).
+              </p>
+            </li>
+
+            <li>
+              Liabilities
+              <p>
+                ArbStore will be exempted from any liability arising from the
+                Transaction or the resolution of the Dispute by the Arbitrator.
+                ArbStore shall not be liable for any loss or damages caused by
+                the Arbitrator to the Parties and by the Parties to each other
+                and to the Arbitrator. ArbStore will make reasonable efforts to
+                ensure that the smart contract and the dispute resolution
+                process work smoothly.
+              </p>
+              <p>
+                Should any of the Parties consider that the Arbitrator has not
+                acted independently, diligently or in good faith, such party
+                should communicate this to ArbStore and ArbStore shall initiate
+                the corresponding inquiry and appoint a moderator in accordance
+                with the Guide.
+              </p>
+              <p>
+                The Parties shall be entitled to initiate legal action against
+                the Arbitrator if the Arbitrator has acted in bad faith,
+                conflict of interests, with negligence or in breach of his/her
+                representations under these Terms and Conditions.
+              </p>
+            </li>
+            <li>
+              No Invalidation
+              <p>
+                Violation of, or failure to uphold, any single article of these
+                Terms and Conditions shall not constitute cancellation or waiver
+                of any additional articles of these Terms and Conditions.
+              </p>
+            </li>
+            <li>
+              Applicable Law and dispute resolution.
+              <p>
+                These Terms and Conditions are subject to Spanish law. Any
+                dispute, controversy or claim arising out of or relating to
+                these Terms and Conditions, shall be resolved by the courts of
+                Barcelona.
+              </p>
+            </li>
+            <li>
+              Acceptance of Terms and Conditions.
+              <p>
+                By using contracts with arbitration and selecting an Arbitrator
+                listed on the ArbStore, the Parties accept these Terms and
+                Conditions, including the <router-link to="/privacy">Privacy statement</router-link>. Arbitrator accepts
+                these Terms and Conditions and <router-link to="/arbiter-code-of-conduct">ArbStore’s Code of Conduct for
+                Arbitrators</router-link>, including the <router-link to="/privacy">Privacy statement</router-link>, by registering on
+                the ArbStore.
+              </p>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
