1278040b9d75cb4a7d04003b6fa6f101868562f9
tony <tonyofbyteball@users.noreply.github.com>
2023-08-18 19:23:54 +0500

    in/out text

--- src/views/Fiat-in-out.vue
+++ src/views/Fiat-in-out.vue
@@ -16,13 +16,25 @@
       </div>
       <div class="row">
         <div class="col-lg-9 f-18 mt-2 color-2">
-          To convert your fiat to USDC or other cryptocurrencies, use any cryptocurrency exchange. Depending on your jurisdiction, different exchanges might be available to you. Popular choices are Coinbase, Binance, Kraken, Bittrex, and many others.
+          To convert your fiat to USDC or other cryptocurrencies, use any cryptocurrency exchange. Depending on your jurisdiction, different exchanges might be available to you. Popular choices are 
+          <a href="https://coinbase.com/" target="_blank" rel="noopener nofollow">Coinbase</a>, 
+          <a href="https://binance.com/" target="_blank" rel="noopener nofollow">Binance</a>, 
+          <a href="https://kraken.com/" target="_blank" rel="noopener nofollow">Kraken</a>, 
+          <a href="https://crypto.com/" target="_blank" rel="noopener nofollow">Crypto.com</a>, 
+          <a href="https://global.bittrex.com/" target="_blank" rel="noopener nofollow">Bittrex</a>, 
+          and many others.
+        </div>
+        <div class="col-lg-9 f-18 mt-4 color-2">
+          As an alternative to cryptocurrency exchanges, you can also use OTC desks. A good list of OTC desks working with crypto is available <a href="https://dezentralizedfinance.com/crypto-otc-trading/" target="_blank" rel="noopener nofollow">here</a>. They offer more personalized service that might be more suitable for newcomers, however they work only with large amounts with typical minimums at $50K-100K.
+        </div>
+        <div class="col-lg-9 f-18 mt-4 color-2">
+          Your USDC will be initially on Ethereum, and to move the coins you also need to acquire a small (about $50 worth) amount of ETH since ETH is required to pay for transaction fees on Ethereum. You can buy ETH on any cryptocurrency exchange including those listed above.
         </div>
         <div class="col-lg-9 f-18 mt-4 color-2">
           After obtaining USDC, transfer the coins to your <a href="https://obyte.org" target="_blank" rel="noopener">Obyte wallet</a> using <a href="https://getmein.ooo" target="_blank" rel="noopener">Get Me In</a> or <a href="https://counterstake.org" target="_blank" rel="noopener">Counterstake Bridge</a>.
         </div>
         <div class="col-lg-9 f-18 mt-4 color-2">
-          You'll also need a small (under $1 worth) amount of Bytes to send transactions on Obyte, even if you are seller. You can acquire them on <a href="https://getmein.ooo" target="_blank" rel="noopener">Get Me In</a> or <a href="https://global.bittrex.com/trade/gbyte-btc" target="_blank" rel="noopener">Bittrex</a>.
+          You'll also need a small (under $1 worth) amount of Bytes to send transactions on Obyte, even if you are seller. You can acquire them on <a href="https://getmein.ooo" target="_blank" rel="noopener">Get Me In</a> or <a href="https://global.bittrex.com/trade/gbyte-btc" target="_blank" rel="noopener nofollow">Bittrex</a>.
         </div>
       </div>
       <div class="row">
@@ -32,7 +44,7 @@
       </div>
       <div class="row">
         <div class="col-lg-9 f-18 mt-2 color-2">
-          If you are a seller and have received stablecoins from a smart contract, you need to do the reverse operations to get fiat money: transfer the coins from Obyte to Ethereum using <a href="https://counterstake.org" target="_blank" rel="noopener">Counterstake Bridge</a>, then sell them for fiat on any cryptocurrency exchange, and withdraw fiat to your bank account.
+          If you are a seller and have received stablecoins from a smart contract, you need to do the reverse operations to get fiat money: transfer the coins from Obyte to Ethereum using <a href="https://counterstake.org" target="_blank" rel="noopener">Counterstake Bridge</a>, then sell them for fiat on any cryptocurrency exchange (or OTC desk), and withdraw fiat to your bank account.
         </div>
         <div class="col-lg-9 f-18 mt-4 color-2">
           If you are both a buyer and a seller in different contracts (with suppliers and customers), you don't need to go to fiat all too often and can save on conversion costs.
